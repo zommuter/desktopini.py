55829bc79048be3245c3e1fa0a6933589dd32d5a
Tobias Kienzler <zommuter@users.noreply.github.com>
2016-03-02 13:55:27 +0100

    activate_desktop_ini

--- desktopini.py
+++ desktopini.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+import win32con, win32api, os
+
+def activate_desktop_ini(dirname):
+    dektopini = os.path.join(dirname, "desktop.ini")
+    with open(dektopini, 'a'):
+        pass # just make sure desktop.ini already exists
+    win32api.SetFileAttributes(dektopini, win32con.FILE_ATTRIBUTE_HIDDEN | win32con.FILE_ATTRIBUTE_SYSTEM)
+    win32api.SetFileAttributes(dirname, win32con.FILE_ATTRIBUTE_READONLY)
+
