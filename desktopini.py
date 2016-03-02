bd216a8eab9ddc256e0c7622eaad75a2f58cae67
Tobias Kienzler <zommuter@users.noreply.github.com>
2016-03-02 14:00:50 +0100

    refactoring get_desktop_ini; read_desktop_ini; fix typo

--- desktopini.py
+++ desktopini.py
@@ -2,11 +2,19 @@
 # -*- coding: utf-8 -*-
 
 import win32con, win32api, os
+from ConfigParser import RawConfigParser
+
+def get_desktop_ini(dirname):
+    desktopini = os.path.join(dirname, "desktop.ini")
+    with open(desktopini, 'a'):
+        pass # just make sure desktop.ini already exists
+    return desktopini
 
 def activate_desktop_ini(dirname):
-    dektopini = os.path.join(dirname, "desktop.ini")
-    with open(dektopini, 'a'):
-        pass # just make sure desktop.ini already exists
-    win32api.SetFileAttributes(dektopini, win32con.FILE_ATTRIBUTE_HIDDEN | win32con.FILE_ATTRIBUTE_SYSTEM)
+    desktopini = get_desktop_ini(dirname)
+    win32api.SetFileAttributes(desktopini, win32con.FILE_ATTRIBUTE_HIDDEN | win32con.FILE_ATTRIBUTE_SYSTEM)
     win32api.SetFileAttributes(dirname, win32con.FILE_ATTRIBUTE_READONLY)
 
+def read_desktop_ini(dirname):
+    desktopini = get_desktop_ini(dirname)
+    return RawConfigParser().read(desktopini)
